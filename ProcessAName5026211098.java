fc2c7f3e14767e1c3ce8c40500e30dc725b77d1a
Bayu Liano Leader Habibullah <95364030+bayuliano@users.noreply.github.com>
2022-02-25 12:27:27 +0700

    ProcessAName5026211098.java
    Simulate the rolling of two 6-sided dice and display their sum.

---
+++ ProcessAName5026211098.java
@@ -0,0 +1,19 @@
+package ProcessAName5026211098.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int dadu1;
+        int dadu2;
+        int duaDadu;
+
+        dadu1 = (int)(Math.random()*6) + 1;
+        dadu2 = (int)(Math.random()*6) + 1;
+        duaDadu = dadu1 + dadu2;
+
+        System.out.println("angka yang muncul di dadu 1 adalah = " + dadu1);
+        System.out.println("angka yang muncul di dadu 2 adalah = " + dadu2);
+        System.out.println("Jumlah kedua dadu adalah = " + duaDadu);
+
+    }
+}
